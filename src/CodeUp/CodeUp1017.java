ea42e6046a6d0148d621c67666cd958f783e7d88
DookongE <108391517+DookongE@users.noreply.github.com>
2022-07-01 18:54:09 +0900

    dddd

--- src/CodeUp/CodeUp1017.java
+++ src/CodeUp/CodeUp1017.java
@@ -6,7 +6,7 @@ public class CodeUp1017 {
 		System.out.println("정수를 하나 입력하세요: ");
 		Scanner sc = new Scanner(System.in);
 		int a = sc.nextInt();
-		System.out.print(a +  " " + a + " " + a);
+		System.out.print(a + " " + a + " " + a);
 		sc.close();
 	}
 }
