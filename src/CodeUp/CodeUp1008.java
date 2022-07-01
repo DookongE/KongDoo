8b368e3c18b4952a1dc75103b63a8f88ea2dedb1
DookongE <108391517+DookongE@users.noreply.github.com>
2022-07-02 03:24:58 +0900

    Update CodeUp1008.java

--- src/CodeUp/CodeUp1008.java
+++ src/CodeUp/CodeUp1008.java
@@ -2,8 +2,8 @@ package CodeUp;
 
 public class CodeUp1008 {
 	public static void main(String[]args) {
-		System.out.println("¦£¦¨¦¤");
-		System.out.println("¦§¦«¦©");
-		System.out.println("¦¦¦ª¦¥");
+		System.out.println("\u250C\u252C\u2510\n");
+		System.out.println("\u251C\u253C\u2524\n");
+		System.out.println("\u2514\u2534\u2518\n");
 	}
 }
