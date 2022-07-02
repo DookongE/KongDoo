88c15023ceaacc6929a846eeb6db7748c4234d9e
DookongE <108391517+DookongE@users.noreply.github.com>
2022-07-03 03:06:31 +0900

    22/07/02

---
+++ src/CodeUp/CodeUp1021.java
@@ -0,0 +1,12 @@
+package CodeUp;
+import java.util.Scanner;
+
+public class CodeUp1021 {
+	public static void main(String[]args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("단어를 입력하세요: ");
+		String name = sc.next();
+		System.out.print(name);
+		sc.close();
+	}
+}
