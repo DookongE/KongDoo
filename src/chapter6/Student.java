c2e979435b4473435a158d3237a2524461dff538
DookongE <108391517+DookongE@users.noreply.github.com>
2022-07-03 18:33:01 +0900

    22/07/03

--- src/chapter6/Student.java
+++ src/chapter6/Student.java
@@ -2,8 +2,24 @@ package chapter6;
 
 public class Student {
 	
-	static int serialNum = 10000;
+	private static int serialNum = 10000; //바꾸면 안되서 private으로 선언
 	
 	int studentId;
 	String studentName;
+	
+	public Student() {
+		serialNum++;
+		studentId = serialNum;
+	}
+	
+	public static int getSerialNum() { //static 메서드에서는 인스터스 변수를 사용할 수 없다.
+		int i = 10; // 이건 지역변수 사용가능
+		
+		i++;
+		System.out.println(i);
+		
+		//studentName = "홍길동"; // 멤버변수, 인스턴스 변수 *생성되지도 않은 인스턴스 변수를 사용하게 되는 것임으로 오류가 남.
+		return serialNum; // static 변수, 클래스 변수
+		
+	}
 }
--- src/chapter6/studentTest1.java
+++ src/chapter6/studentTest1.java
@@ -2,14 +2,15 @@ package chapter6;
 
 public class studentTest1 {
 	public static void main(String[] args) {
+		
+		System.out.println(Student.getSerialNum());
+		
 		Student studentJ = new Student();
-		System.out.println(studentJ.serialNum);
-		studentJ.serialNum++;
-		
-		
 		Student studentT = new Student();
+		System.out.println(studentJ.studentId);
+		System.out.println(studentT.studentId);
 		
-		System.out.println(studentJ.serialNum);
-		System.out.println(studentT.serialNum);
+		
+		
 	}		
 }
